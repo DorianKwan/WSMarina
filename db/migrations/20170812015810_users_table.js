09c3a3666567b39f4ee80742c9dd13332e0eeccf
Bryce <bryce.sayers-kwan@live.ca>
2017-08-12 04:15:53 +0000

    Added migration for users table

---
+++ db/migrations/20170812015810_users_table.js
@@ -0,0 +1,18 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary();
+    table.string('username', 16).unique().notNullable();
+    table.string('email', 40).notNullable();
+    table.string('password_digest').notNullable();
+    table.text('bio');
+    table.integer('rep');
+    table.date('date_of_birth');
+    table.string('image');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('users')
+  ]);
+};
