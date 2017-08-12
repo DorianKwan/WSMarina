8fca7cb57b2c65b97537c250d7eb2bd289ef9bba
Yelim Oh <yelim.oh1@gmail.com>
2017-08-12 06:10:34 +0000

    Refactor class Register and Login to include event-handler functions

--- modules/Register.jsx
+++ modules/Register.jsx
@@ -1,40 +1,65 @@
 import React from 'react'
 
-export default React.createClass({
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      bio: '',
+      birthday: '',
+      photo: ''
+  };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({name: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <h3>Sign Up!</h3>
         <label>
           Full Name:
-          <input type="text" name="name" />
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
         </label>
         <label>
           Email:
-          <input type="text" name="email" />
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
         </label>
         <label>
           Password:
-          <input type="text" name="password" />
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
         </label>
         <label>
           Password Confirmation:
-          <input type="text" name="password_confirmation" />
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
         </label>
         <label>
           Bio:
-          <input type="text" name="bio" />
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
         </label>
         <label>
           Birthday:
-          <input type="date" name="birthday" />
+          <input type="date" value={this.state.value} onChange={this.handleChange} />
         </label>
         <label>
           Upload A Profile Photo:
-          <input type="file" name="profile_image" />
+          <input type="file" value={this.state.value} onChange={this.handleChange} />
         </label>
         <input type="submit" value="Submit" />
       </form>
     );
   }
-})
+}
+
+export default Register;
