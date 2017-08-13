d5ba3076f94fff63f25cf0643ad0ff80d954cf67
rajmytaj <chalasanidheeraj@gmail.com>
2017-08-13 00:09:15 +0000

    modified chatrooms table, added chatroom_users, bets, stocks and payouts tables

--- future_migrations.js
+++ future_migrations.js
@@ -2,9 +2,9 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTable('messages', function(table){
-      table.uuid('id');
+      table.uuid('id').primary();
       table.text('content');
-      table.foreign('user_id').references('chatrooms.id');
+      table.foreign('user_id').references('users.id');
       table.date('timestamp');
       table.foreign('chatroom_id').references('chatrooms.id');
     })
@@ -17,12 +17,12 @@ exports.down = function(knex, Promise) {
   ])
 };
 
-// -- Chatrooms
+// -- chatrooms
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTable('chatrooms', function(table){
-      table.uuid('id');
-      table.string('name', 16);
+      table.uuid('id').primary();
+      table.string('chatroom_name', 16);
     })
   ])
 };
@@ -33,16 +33,36 @@ exports.down = function(knex, Promise) {
   ])
 };
 
+// -- chatroom_users
+// can find which user is is what chat here
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('chatroom_users', function(table){
+      table.uuid('id').primary();
+      table.foreign('chatroom_id').references('chatrooms.id');
+      table.foreign('user_id').references('users.id');
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('chatroom_users')
+  ])
+};
+
+
 // -- Bets 
+// list of bets, the stock the bet is on, date of creation of bet
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTable('bets', function(table){
-      table.uuid('id');
-      table.int('rep_wagered').notNullable();
-      table.foreign('pool_id').references('pools.id');
-      table.foreign('user_id').references('users.id');
-      table.int('rep_won').nullable();
-      table.bool('up');
+      table.uuid('id').primary();
+      table.date('date_created').defaultTo(knex.raw('now()'));
+      // date now !!!!!
+      table.foreign('stock_id').references('stocks_id');     
+      // Or
+      // table.string('ticker', 4).notNullable();
     })
   ])
 };
@@ -53,3 +73,39 @@ exports.down = function(knex, Promise) {
   ])
 };
 
+// -- stocks
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('stocks', function(table){
+      table.uuid('id');
+      table.string('company_ticker');
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('stocks')
+  ])
+};
+
+// -- Payouts 
+// 
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('payouts', function(table){
+      table.uuid('id').primary();
+      table.foreign('bet_id').references('bets.id'); 
+      table.date('date_of_payout').defaultTo(knex.raw('now()'));
+      // the above hack should prevent potential errors or change date to dateTime
+      table.foreign('user_id').references('users.id');
+      table.int('amount');
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('payouts')
+  ])
+};
