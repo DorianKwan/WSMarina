986133601fa834b563300eef0cfa3ef1e8b5b52f
Chester Ni <kaichesterni@gmail.com>
2017-08-15 16:48:34 +0000

    Cleaned up modules

--- modules/Login.jsx
+++ modules/Login.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { browserHistory } from 'react-router'
+import React from "react";
+import {browserHistory} from "react-router";
 
 export default React.createClass({
   contextTypes: {
@@ -7,47 +7,41 @@ export default React.createClass({
   },
 
   handleSubmit(event) {
-    event.preventDefault()
-    const email = event.target.elements[0].value
-    const password = event.target.elements[1].value
+    event.preventDefault();
+    const email = event.target.elements[0].value;
+    const password = event.target.elements[1].value;
 
-    const body = JSON.stringify({
-      email: email,
-      password: password
-    })
+    const body = JSON.stringify({email: email, password: password});
 
-    fetch('http://localhost:3000/login', {
-      method: 'POST',
+    fetch("http://localhost:3000/login", {
+      method: "POST",
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Content-Length': new Buffer(body).length
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Content-Length": new Buffer(body).length
       },
       body: body
-    })
-    .then((response) => {
-      console.log(response)
+    }).then((response) => {
+      console.log(response);
       if (response.status === 200) {
-        alert('Logged in successfully!');
+        alert("Logged in successfully!");
       } else if (response.status === 409) {
-        alert('Bad credentials!');
+        alert("Bad credentials!");
       } else if (response.status === 410) {
-        alert('Email or password cannot be blank!');
+        alert("Email or password cannot be blank!");
       } else {
-        alert('Something went wrong!');
+        alert("Something went wrong!");
       }
-    })
+    });
   },
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <input type="text" placeholder="email" /> {' '}
-        <input type="password" placeholder="password" />{' '}
+        <input type="text" placeholder="email"/> {" "}
+        <input type="password" placeholder="password"/>{" "}
         <button type="submit">Log in</button>
       </form>
     );
   }
 });
-
-
--- modules/index.jsx
+++ modules/index.jsx
@@ -1,10 +1,8 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { Router, browserHistory } from 'react-router';
-// import routes and pass them into <Router/>
-import routes from './routes.jsx';
+import React from "react";
+import routes from "./routes.jsx";
+import {render} from "react-dom";
+import {Router, browserHistory} from "react-router";
 
 render(
-  <Router routes={routes} history={browserHistory} />,
-  document.getElementById('app')
+  <Router routes={routes} history={browserHistory}/>, document.getElementById("app")
 );
