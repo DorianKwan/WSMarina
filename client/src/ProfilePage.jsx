da60f05a6449c92ecec7455216cffa733a3ca627
rajmytaj <chalasanidheeraj@gmail.com>
2017-08-17 11:34:30 -0700

    reverting profile page content

--- client/src/ProfilePage.jsx
+++ client/src/ProfilePage.jsx
@@ -8,11 +8,58 @@ class ProfilePage extends React.Component {
       bio:'',
     }
   }
+  getInitialState: function(params) {
+    return {
+      users: {}
+    }
+  }
+
+  componentDidMount() {
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    const email = event.target.elements[0].value;
+    const password = event.target.elements[1].value;
+
+    const body = JSON.stringify({email: email, password: password});
+
+    fetch("", {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Content-Length": new Buffer(body).length
+      },
+      body: body
+    }).then((response) => {
+      console.log(response);
+      if (response.status === 200) {
+        alert("profile updated!");
+      }  
+    });
+  },
+
   render() {
     return (
+      <div>
+        <div className="profile-form">
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" placeholder="email"/> {" "}
+            <input type="password" placeholder="password"/>{" "}
+            <button type="submit">Log in</button>
+          </form>
+        </div>
+        <div className="profile-content">
+          <Image src={this.state.image} />
+          <Profile person={this.state.person} quote={this.state.quote} />
+        </div>
+      </div>
       <div className="ProfilePage">
         <h2>ProfilePage</h2>
-
+        <div>
+          <img src={this.state.avatarURL}/> 
+        </div>
+         </div>
       </div>
     );
   }
