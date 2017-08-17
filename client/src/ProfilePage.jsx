357e2ca7dee2a3693f222203cda538af5cf1f532
rajmytaj <chalasanidheeraj@gmail.com>
2017-08-17 13:36:34 -0700

    removed zombie code

--- client/src/ProfilePage.jsx
+++ client/src/ProfilePage.jsx
@@ -2,28 +2,24 @@ import React from 'react';
 
 class ProfilePage extends React.Component {
   constructor(props){
-    super(props);
-    this.state={
+    super(props); 
+    this.state = {
       avatarUrl:'',
       bio:'',
     }
+    this.onSubmit = this.handleSubmit.bind(this);
   }
-  // getInitialState: function(params) {
-  //   return {
-  //     users: {}
-  //   }
-  // }
 
   componentDidMount() {
   }
+
   handleSubmit(event) {
     event.preventDefault();
     const email = event.target.elements[0].value;
     const password = event.target.elements[1].value;
+    const body = JSON.stringify({avatarUrl: avatarUrl, bio: bio});
 
-    const body = JSON.stringify({email: email, password: password});
-
-    fetch("", {
+    fetch("/users", {
       method: "POST",
       headers: {
         "Accept": "application/json",
@@ -31,36 +27,23 @@ class ProfilePage extends React.Component {
         "Content-Length": new Buffer(body).length
       },
       body: body
-    }).then((response) => {
-      console.log(response);
-      if (response.status === 200) {
-        alert("profile updated!");
-      }  
+    })
+    .then((response) => {
+      return response.json()
+    }).then((body) => {
+      console.log(body);
     });
   }
 
   render() {
     return (
       <div>
-        {/* <div className="profile-form">
-          <form onSubmit={this.handleSubmit}>
-            <input type="text" placeholder="email"/> {" "}
-            <input type="password" placeholder="password"/>{" "}
-            <button type="submit">Log in</button>
-          </form>
-        </div>
-        <div className="profile-content">
-          <Image src={this.state.image} />
-          <Profile person={this.state.person} quote={this.state.quote} />
-        </div>
-      </div>
-      <div className="ProfilePage">
-        <h2>ProfilePage</h2>
-        <div>
-          <img src={this.state.avatarURL}/> 
-        </div> */}
         <p> profile page</p>
-         {/* </div> */}
+        <form onSubmit={this.onSubmit}>
+        <input type="text" placeholder="avatarURL" ref="avatarURL"/>
+        <input type="text" placeholder="bio" ref="bio"/>
+        <input type="submit" />
+      </form>
       </div>
     );
   }
