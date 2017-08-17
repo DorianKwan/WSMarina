463007f62a6b6b529af95c9cbacadb731d049bc5
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2017-08-16 22:12:09 -0700

    Merge branch 'master' into feature/tickers

---
+++ client/src/chatroom/ChatNav.jsx
@@ -0,0 +1,16 @@
+
+import React, { Component } from 'react';
+
+// Class chatNav renders nav-bar of page which includes logo and count of online users
+class ChatNav extends Component {
+  render() {
+    return (
+      <nav className="chatNav">
+        <a href="/" className="chatNav-brand">W.S Marina </a>
+        <span className="online-users">{this.props.clientCount} users online</span>
+      </nav>
+    )
+  }
+}
+
+export default ChatNav;
---
+++ client/src/chatroom/Message.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+// Class Message renders new message
+class Message extends Component {
+  render() {
+    switch (this.props.message.type) {
+      // If incomingMessage, render message with username and messsage content
+      case "incomingMessage":
+        return (
+          <div className="message">
+            <span className="message-username">{this.props.message.username}</span>
+            <span className="message-content">{this.props.message.content}</span>
+          </div>
+        );
+        break;
+      // If incomingNotification, will render notification message about username change
+      case "incomingNotification":
+        return (
+          <div className="message-system">
+            {this.props.message.oldName} changed their name to {this.props.message.newName}
+          </div>
+        );
+        break;
+      // Should not reach default due to filter in MessageList
+      // But if so returns error and empty div
+      default:
+        console.log("Error in message");
+        return (<div></div>);
+        break;
+    }
+  }
+}
+export default Message;
