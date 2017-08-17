ef94990c412eb88a1423d23d51d8526a5fbe061a
rajmytaj <chalasanidheeraj@gmail.com>
2017-08-17 15:48:39 -0700

    removed dead code

--- server/routes/users.js
+++ server/routes/users.js
@@ -19,60 +19,6 @@ function createRouter(knex) {
     
   })
 
-  // router.post("/", (req, res) => {
-
-  //   // Check if user input exists
-  //   if (!req.body.email || !req.body.password) {
-  //     res.sendStatus(410);
-  //     return;
-  //   }
-
-  //   // Check if user email is already being used
-  //   knex("users")
-  //     .select("*")
-  //     .where({
-  //       email: req.body.email
-  //     })
-  //     .limit(1)
-  //     .then((rows) => {
-
-  //       const user = rows[0];
-  //       if (!user) {
-  //         return Promise.reject({
-  //           type: 409,
-  //           message: "Email is already being used"
-  //         });
-  //       }
-
-  //       // Check if username is already being used
-  //       const comparePasswords = bcrypt.compare(req.body.password, user.password_digest);
-  //       return comparePasswords.then((passwordsMatch) => {
-
-
-  //         if (!passwordsMatch) {
-  //           return Promise.reject({
-  //             type: 409,
-  //             message: "Username is already being used"
-  //           });
-  //         }
-
-  //         return Promise.resolve(user);
-  //       });
-  //     }).then((user) => {
-
-  //     // Set cookie to reflect logged in status and redirect to users page
-
-  //     req.session.user_id = user.id;
-  //     res.redirect('/');
-
-  //   }).catch((err) => {
-
-  //     // Lazy error handling
-  //     // TODO: properly handle errors
-  //     res.sendStatus(err.type);
-  //   });
-  // });
-
   return router;
 }
 
