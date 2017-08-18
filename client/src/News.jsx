e4a670ada250ac2dc47e60299bb2fb9616c425bd
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2017-08-17 22:58:01 -0700

    Added postArticles function

--- client/src/News.jsx
+++ client/src/News.jsx
@@ -1,9 +1,50 @@
 import React from 'react';
 
 class News extends React.Component {
+
+  postArticles() {
+    console.log('filled with hatred');
+    const newsApiKey = 'Your newsapi key here';
+    fetch(`https://newsapi.org/v1/articles?source=bloomberg&sortBy=top&apiKey=${newsApiKey}`)
+      .then((resp) => resp.json())
+      .then(news => {
+        const articlesList = news.articles;
+        const source = news.source;
+        const articles = articlesList.map(article => {
+          const title = article.title;
+          const author = article.author;
+          const description = article.description;
+          const url = article.url;
+          const image = article.urlToImage;
+          const publishedAt = article.publishedAt;
+          return {
+            title,
+            author,
+            description,
+            url,
+            image,
+            publishedAt,
+            source
+          };
+        });
+        return articles;
+      })
+      .then((articles) => {
+        return orderByTime(articles);
+      })
+      .then((articles) => {
+        this.setState({
+          articles
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   render() {
     return (
-      <div className="news">News</div>
+      <section className="news"></section>
     );
   }
 }
