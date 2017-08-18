738b2fb78391c1669397f08961d50d5ed2e9cad4
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2017-08-18 00:17:07 -0700

    Fixed indentation

--- server/routes/currentUser.js
+++ server/routes/currentUser.js
@@ -4,16 +4,15 @@ function createRouter(knex) {
   const router = express.Router();
 
   router.get("/", (req, res) => {
-    console.log(req.session.user_id);
     knex("users")
       .select("id","username","email","bio","image","rep" )
       .where({
         id: req.session.user_id
       })
       .limit(1)
-    .then((userInfo) => {
-      res.send(userInfo[0]);
-    });
+      .then((userInfo) => {
+        res.send(userInfo[0]);
+      });
   });
   return router;
 }
