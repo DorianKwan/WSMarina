36dd9f15a89a87659cbd5a475f0e3c788f59fe1f
rajmytaj <chalasanidheeraj@gmail.com>
2017-08-19 17:48:32 -0700

    base navbar and relevant css files created/modified

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 class Navbar extends React.Component {
   render() {
     return (
-      <div className="navbar">Navbar</div>
+      <div className="navbar">
+        <ul>
+          <li><a>item1</a></li>
+          <li><a>item1</a></li>
+          <li><a>item1</a></li>
+          <li><a>item1</a></li>          
+        </ul>
+      </div>
     );
   }
 }
