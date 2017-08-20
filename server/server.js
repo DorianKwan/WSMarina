e658097bc7b54bcaa0e7e5b40e83dca0c886a6fa
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2017-08-20 13:26:15 -0700

    Added new farmReset route to server

--- server/server.js
+++ server/server.js
@@ -20,6 +20,7 @@ const flairsRouter = require('./routes/flairs');
 const currentUserFlairsRouter = require('./routes/currentUserFlairs');
 const leadersRouter = require('./routes/leaders');
 const farmsRouter = require('./routes/farms');
+const farmResetRouter = require('./routes/farmReset');
 
 app.set('view engine', 'ejs');
 
@@ -61,6 +62,7 @@ app.use('/flairs', flairsRouter(knex));
 app.use('/currentUserFlairs', currentUserFlairsRouter(knex));
 app.use('/leaders', leadersRouter(knex));
 app.use('/farms', farmsRouter(knex));
+app.use('/reset', farmResetRouter(knex));
 
 function broadcast(data) {
   if (data.type === "userCount") {
