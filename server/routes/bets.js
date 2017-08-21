1cdf945085a27a93cc1cac1a2e01fefc0adc7499
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2017-08-20 19:41:54 -0700

    Created router for get and post requests to bets table

---
+++ server/routes/bets.js
@@ -0,0 +1,59 @@
+const express = require("express");
+
+function createRouter(knex) {
+  const router = express.Router();
+  router.get("/", (req, res) => {
+
+    knex("bets")
+      .where("user_id", req.session.user_id)
+      .select("ticker", "wager", "direction")
+      .then((bets) => {
+        res.send(bets[0]);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json({"error": "Error has occured."});
+      });
+  });
+
+  router.post("/", (req, res) => {
+
+    const { ticker, wager, direction } = req.body;
+
+    // Check if user entered ticker, wager, direction
+    if (!ticker || !wager || direction === null) {
+      req.flash('errors', 'Input fields cannot be empty');
+    }
+
+    // Check if a bet is already placed on selected ticker
+    knex("bets")
+      .select(1)
+      .where("user_id", req.session.user_id)
+      .limit(1)
+      .then((bets) => {
+
+        if (bets.length) {
+          return Promise.reject({
+            message: 'A bet for this stock has already been placed'
+          });
+        }
+        
+      }).then(() => {
+
+        knex("bets")
+          .insert({
+            ticker,
+            wager,
+            direction
+          });
+
+      }).catch((err) => {
+        req.flash('errors', err.message);
+        res.redirect("/");
+      });
+
+  });
+  return router;
+}
+
+module.exports = createRouter;
