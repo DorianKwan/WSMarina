7f28ca77229660ff1731f10f87945f92aa1a7327
rajmytaj <chalasanidheeraj@gmail.com>
2017-08-21 14:27:17 -0700

    switched around page title and navlink

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -7,14 +7,14 @@ class Navbar extends React.Component {
       <nav className="navbar has-shadow">
         <div className="container">
           <div className="nav-left">
+          <a className="nav-item is-tab" id="site-name"> W.S. MARINA</a>            
+          </div>
+
+          <div className="nav-right nav-menu">
             <a className="nav-item is-tab is-active"> &nbsp; HOME</a>
             <a className="nav-item is-tab"> &nbsp; FARM</a>
             <a className="nav-item is-tab"> &nbsp; BETS</a>
             <a className="nav-item is-tab"> &nbsp; STORE</a>
-          </div>
-
-          <div className="nav-right nav-menu">
-            <a className="nav-item" id="site-name"> W.S. MARINA</a>
           </div>
         </div>
       </nav>
