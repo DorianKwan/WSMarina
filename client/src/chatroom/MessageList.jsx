eb4d40f3f3928945d4c28ba87a6c3bbc68146b29
Yelim Oh <yelim.oh1@gmail.com>
2017-08-24 14:58:30 -0700

    Add scroll down to latest message in MessageList component

--- client/src/chatroom/MessageList.jsx
+++ client/src/chatroom/MessageList.jsx
@@ -6,9 +6,23 @@ class MessageList extends Component {
   constructor(props) {
     super(props);
   }
+
+  scrollToBottom() {
+    const scrollHeight = this.messages.scrollHeight;
+    const height = this.messages.clientHeight;
+    const maxScrollTop = scrollHeight - height;
+    this.messages.scrollTop = maxScrollTop > 0 ? maxScrollTop : 0;
+  }
+
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+
   render() {
     return (
-      <main className="messages">
+      <main className="messages" ref={(div) => {
+          this.messages = div;
+        }}>
         {
           // Filter ensures that only messages with type incomingMessage gets rendered
           this.props.messages
